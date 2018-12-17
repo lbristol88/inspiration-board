59ebd999b9d48416f844881a73adfde9d6463437
Layla Bristol <layla.bristol@gmail.com>
2018-12-17 09:36:28 -0800

    adding test files

---
+++ src/components/test/Card.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Card from '../Card';
+import { shallow } from 'enzyme';
+
+
+
+ describe('Card', () => {
+   
+  test('that it matches an existing snapshot', () => {
+
+    const wrapper = shallow(
+      <Card
+        text="test text"
+        emoji="test emoji"
+        />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
+});
